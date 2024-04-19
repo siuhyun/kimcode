ae496e069738bff6955373157a1c6962413c23d0
Kimcode <sophy0219@naver.com>
2024-04-19 17:16:13 +0900

    최종본

--- js/main.js
+++ js/main.js
@@ -1,68 +1,116 @@
-// let projectSection = document.querySelector('#project').offsetTop;
-// // console.log(projectSection); //1870
-// window.addEventListener('scroll', () => {
-//   // console.log(window.scrollX, window.scrollY);
-//   if ()
-// })
+const header = document.querySelector('header')
+const nav_li = document.querySelectorAll('nav li a')
+let headerHeight = header.offsetHeight;
+window.addEventListener('scroll', function(){
+  let windowTop = window.scrollY;
 
-// const navEl = document.querySelector('#project nav')
-// const navTop = navEl.getBoundingClientRect().top + window.scrollY;
-// console.log(navTop);
-// window.addEventListener('scroll', function(){
-//   if(window.scrollY >= navTop){
-//     navEl.classList.add('fixed');
-//   }
-// });
-/* scroll마다 한 페이지씩 이동하기 */
-window.onload = function () {
-  const navEl = document.querySelector('#project nav')
-  const navTop = navEl.getBoundingClientRect().top + window.scrollY;
-  const elm = document.querySelectorAll("#project section");
-  const elmCount = elm.length;
-  
-  window.addEventListener('scroll', function(){
-    if(window.scrollY >= navTop){
-      navEl.classList.add('fixed');
-    }
+  if(windowTop >= headerHeight) {
+    header.classList.add('drop')
+  } else {
+    header.classList.remove('drop')
+  }
+})
+
+gsap.registerPlugin(ScrollTrigger);
+
+
+//visual 수정
+
+const visual = gsap.timeline()
+visual.from(".visual .text_box", { scale: 0.3, rotation: 45, autoAlpha: 0 })
+      .to(".visual .text_box", { scale: 1, rotation: 0, autoAlpha: 1 })
+
+
+//GSAP - profile
+
+gsap.to(".profile", {
+  scrollTrigger: ".profile",
+});
+let tl = gsap.timeline({
+
+  scrollTrigger: {
+    trigger: ".profile",
+    start: "-20% 40%",
+    end: "+=500",
+    scrub: 1,
+    snap: {
+      snapTo: "labels",
+      duration: { min: 0.2, max: 3 },
+      delay: 0.2,
+      ease: "power1.inOut",
+    },
+  },
+});
+
+// add animations and labels to the timeline
+
+tl.addLabel("start")
+  .from(".profile .img_box", { scale: 0.3, rotation: 45, autoAlpha: 0 })
+  .addLabel("end");
+
+  const ani1 = gsap.timeline();
+  ani1 .to("#project nav", {y:0})
+
+  ScrollTrigger.create({
+      animation: ani1,
+      trigger: "#project nav",
+      start: "top top",
+      endTrigger: "#project",
+      end: "bottom bottom",
+      scrub: true,
+      pin: true,
+      anticipatePin: 1,
   });
-  elm.forEach(function (item, index) {
-    item.addEventListener("mousewheel", function (event) {
-      event.preventDefault();
-      let delta = 0;
-      if (!event) event = window.event;
-      if (event.wheelDelta) {
-        delta = event.wheelDelta / 120;
-        if (window.opera) delta = -delta;
-      } else if (event.detail) delta = -event.detail / 3;
 
-      let moveTop = window.scrollY;
-      let elmSelector = elm[index];
 
-      // wheel down : move to next section
-      if (delta < 0) {
-        if (elmSelector !== elmCount - 1) {
-          try {
-            moveTop =
-              window.pageYOffset +
-              elmSelector.nextElementSibling.getBoundingClientRect().top;
-          } catch (e) {}
-        }
+  const scrollLinks = document.querySelectorAll('#project nav a');
+  const projectStart = document.querySelector('#project').offsetTop
+  console.log(projectStart);
+
+
+  scrollLinks.forEach(function(link) {
+    link.addEventListener('click', function(e) {
+      e.preventDefault();
+
+      const targetId = this.getAttribute('href');
+      const targetSection = document.querySelector(targetId);
+
+      if (targetSection) {
+        const targetOffset = projectStart + targetSection.offsetTop;
+
+        console.log(targetOffset);
+
+        window.scrollTo({
+          top: targetOffset,
+          behavior: 'smooth'
+        });
+        scrollLinks.forEach(function(link) {
+          link.classList.remove('nav__menu--foused');
+        });
+
+        this.classList.add('nav__menu--foused');
       }
-
-      // wheel up : move to previous section
-      else {
-        if (elmSelector !== 0 && elmSelector.id !== "home") {
-          try {
-            moveTop =
-              window.pageYOffset +
-              elmSelector.previousElementSibling.getBoundingClientRect().top;
-          } catch (e) {}
-        }
-      }
-
-      const body = document.querySelector("html");
-      window.scrollTo({ top: moveTop, left: 0, behavior: "smooth" });
     });
   });
-};
 
+  window.addEventListener('scroll', function() {
+    const scrollPosition = window.scrollY;
+
+    document.querySelectorAll('section').forEach(function(section) {
+      const sectionId = '#' + section.getAttribute('id');
+      const sectionOffset = section.offsetTop +projectStart;
+      const sectionHeight = section.offsetHeight;
+
+      if (scrollPosition >= sectionOffset && scrollPosition < sectionOffset + sectionHeight) {
+
+        document.querySelector('a[href="' + sectionId + '"]').classList.add('nav__menu--foused');
+      } else {
+        document.querySelector('a[href="' + sectionId + '"]').classList.remove('nav__menu--foused');
+      }
+    });
+  });
+
+  // jQuery
+  $('#toggleNav').click(function() {
+    $('nav ul').toggleClass('active');
+  });
